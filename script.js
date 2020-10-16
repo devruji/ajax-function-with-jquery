f9f69b445a5ddecc834b0aa53b35d0787f9fb5a3
Satanism <en.rujikorn@gmail.com>
2020-10-16 19:54:31 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+${document}.ready(function() {
+    const $body = $('body');
+    const $list = $('.m-info');
+    const $message = $('.m-message');
+    const $modal = $('.modal');
+    const $loader = $('.loader');
+    const $framework
+
+    @27.45
+    --> https://www.youtube.com/watch?v=luHEL_6N6Rg
+})
